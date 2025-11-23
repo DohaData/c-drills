b57bc83e7e0c56337fbca0e3d3088ebe915306d0
DohaData <doha.kaddaf@gmail.com>
2025-11-23 15:39:46 +0100

    sometimes internal functiom in c++ use function pointers

---
+++ functions-pointers/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void Print(int count, char ch) {
+  for (int i = 0; i < count; i++)
+    std::cout << ch << std::endl;
+}
+
+void EndMessage() { std::cout << "End of Program" << std::endl; }
+
+int main() {
+  //   Print(5, '#');
+  void (*pfn)(int, char) = &Print; // & is optionnal
+  (*pfn)(8, '@');
+  pfn(5, '*');
+  atexit(EndMessage);
+  return 0;
+}
